c399ce7300de7ddc431c184cd6cc4a8868be369b
lehm1091 <lehm1091@gmail.com>
2020-09-11 13:04:22 -0600

    1 file modified

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -107,9 +107,6 @@ export class UsersService {
     }
     return new Observable((observer: Observer<any>) => {
       observer.next(response);
-      if (response.length === 0) {
-        observer.next("user list didnt existed in local storage");
-      }
     });
 
   }
