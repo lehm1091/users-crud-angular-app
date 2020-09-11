670427223f31bd66cd063bcdad5e938f66e4cf85
Luis H D <lehm1091@gmail.com>
2020-09-11 08:49:00 -0600

    still more fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UsersListComponent } from './components/users-list/users-list.component';
 
-const routes: Routes = [ { path: 'users', component: UsersListComponent },];
+const routes: Routes = [ { path: 'users', component: UsersListComponent },
+{ path: '', redirectTo: 'users', pathMatch: 'full' }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
